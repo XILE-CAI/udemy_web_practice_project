4b22e3774c3296f9ba4115c42c87bdbd972b9336
XILE-CAI <c358143455@gmail.com>
2022-03-28 12:36:54 +1030

    search bar

---
+++ searchbar/script.js
@@ -0,0 +1,20 @@
+const searchInputWrapper = document.querySelector('.search-input-wrapper');
+
+const searchIcon = document.querySelector('.search-icon i');
+const searchInput = document.querySelector('.search-input input');
+const closeIcon = document.querySelector('.search-input i');
+
+searchIcon.addEventListener('click', ()=> {
+    searchIcon.parentElement.classList.add('change');
+    searchInputWrapper.classList.add('change');
+
+    setTimeout(() => {
+        searchInput.foucs();
+    },1000);
+});
+
+closeIcon.addEventListener('click', () => {
+    searchIcon.parentElement.classList.remove('change');
+    searchInputWrapper.classList.remove('change');
+})
+
